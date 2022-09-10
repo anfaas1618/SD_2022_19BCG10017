7bcea95124b242eaf948b98818dcd875d9efda61
anfaas1618 <ishad0911@gmail.com>
2022-09-10 17:30:04 +0530

    create and initialize board

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,29 @@
 public class Game {
+   static final int boardWidth=5;
+   static final  int boardHeight=5;
+
+   static String[][] board = new String[boardWidth][boardHeight];
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        initBoard();
+        draw();
+
+    }
+
+    private static void initBoard() {
+        for (int i = 0; i < boardHeight; i++) {
+            for (int j = 0; j <boardWidth ; j++) {
+                board[i][j]="-";
+            }
+        }
+    }
+
+    private static void draw() {
+        for (int i = 0; i < boardHeight; i++) {
+            for (int j = 0; j <boardWidth ; j++) {
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
